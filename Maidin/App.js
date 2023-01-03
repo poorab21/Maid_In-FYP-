3800242d85a149be2ad8b5dffb1e03cde94902e7
inamsaif0 <inamsaif0>
2023-01-03 12:18:37 +0500

    feat: adding code for refference page

--- Maidin/App.js
+++ Maidin/App.js
@@ -1,37 +1,176 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import {
-  StyleSheet,
-  Text,
-  View,
+
+
+  Modal,
+  Pressable,
   Image,
   TextInput,
   Button,
   TouchableOpacity,
+  Box,
 } from "react-native";
 export default function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.box}>
-          <Text styles={styles.text}>Reffereces</Text>
-      </View> 
-    </View> 
+        <View styles={styles.heading}>
+          <Text style={styles.text}>Refferences</Text>
+        </View>
+        <View style={styles.list}>
+          <View style={styles.contain}>
+            <FlatList
+              data={[
+                { key: 'Devin' },
+                { key: 'Dan' },
+                { key: 'Dominic' },
+                { key: 'Jackson' },
+                { key: 'James' },
+                { key: 'Joel' },
+                { key: 'John' },
+                { key: 'Jillian' },
+                { key: 'Jimmy' },
+                { key: 'Julie' },
+              ]}
+              renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+            />
+          </View>
+
+        </View>
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Hello World!</Text>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+          <Pressable
+            style={[styles.button, styles.buttonOpen]}
+            onPress={() => setModalVisible(true)}
+          >
+            <Text style={styles.textStyle}>Show Modal</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#00ffff",
+    backgroundColor: "#dcdcdc",
     alignItems: "center",
     justifyContent: "center",
+
   },
   box: {
     width: "70%",
-    height:"40%",
+    height: "70%",
     backgroundColor: "#fff8dc",
     alignItems: "center",
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden',
+  },
+  heading: {
+    width: "70%",
+    height: "40%",
+
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+
+  },
+  list: {
+    width: "80%",
+    height: "78%",
+    backgroundColor: "#dcdcdc",
+    marginTop: 20,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden',
+  },
+  contain: {
+    flex: 1,
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  button: {
+    marginTop: 10,
+    width: 220,
+
+
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
+
 });
